211e86dbf3c37c15222e18f010b2a434c3c464a9
Craig Knapp <craighknapp@comcast.net>
2025-05-03 05:09:28 -0600

    Improve how no parameters are handled and what happens when zip file name is not given.

--- ZipCmd/Program.cs
+++ ZipCmd/Program.cs
@@ -17,31 +17,37 @@ public static class Program
         var zipCore = new ZipCore(commandArguments);
         var serviceCollection = SetupInitialServices(zipCore);
         var serviceProvider = serviceCollection.BuildServiceProvider();
-        if (commandArguments.Parse<IZipAction>(mainArgument,  serviceProvider.GetServices<IZipAction>()))
+        if (args.Length > 0 && commandArguments.Parse<IZipAction>(mainArgument,  serviceProvider.GetServices<IZipAction>()))
         {
-
-            var filenames = mainArgument.ZipFile.EnsureExtension(".zip").GetFiles(false, true).ToList();
-
-            if (filenames.Count == 1)
+            if (!string.IsNullOrEmpty(mainArgument.ZipFile))
             {
-                using var stream = File.Open(filenames[0], FileMode.OpenOrCreate);
-                using var zipArchiver = new ZipArchiver(stream);
-                zipCore.Archiver = zipArchiver;
-                zipCore.Stream = stream;
-                var zipCommand = serviceProvider.GetService<IZipCommand>();
-                foreach (var option in commandArguments.Options)
+                var filenames = mainArgument.ZipFile.EnsureExtension(".zip").GetFiles(false, true).ToList();
+
+                if (filenames.Count == 1)
                 {
-                    zipCommand?.Execute(option);
+                    using var stream = File.Open(filenames[0], FileMode.OpenOrCreate);
+                    using var zipArchiver = new ZipArchiver(stream);
+                    zipCore.Archiver = zipArchiver;
+                    zipCore.Stream = stream;
+                    var zipCommand = serviceProvider.GetService<IZipCommand>();
+                    foreach (var option in commandArguments.Options)
+                    {
+                        showHelp = false;
+                        zipCommand?.Execute(option);
+                    }
                 }
-                showHelp = false;
+                else
+                {
+                    Console.WriteLine("ZipCmd - ZipFileName pattern does not reference a single filename.\n");
+                    foreach (var filename in filenames)
+                    {
+                        Console.WriteLine(filename);
+                    }
+                }
             }
             else
             {
-                Console.WriteLine("ZipCmd - ZipFileName pattern does not reference a single filename.\n");
-                foreach (var filename in filenames)
-                {
-                    Console.WriteLine(filename);
-                }
+                    Console.WriteLine("No zip filename given.");
             }
         }
         
